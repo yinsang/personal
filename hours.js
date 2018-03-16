fd7e23de8474470147efc8bf1dd5c53945d782d4
银 <love1175180338@gmail.com>
2018-03-16 20:14:38 +0800

    Update hours.js

--- hours.js
+++ hours.js
@@ -1,5 +1,4 @@
 var doms = document.querySelectorAll(".detail-time")
-var obj = [];
 var hours = 0;
 var days = 0;
 console.error('计算');
@@ -12,9 +11,8 @@ for (var i = 0; i < doms.length - 1; i++) {
             var startMin = str.split("-")[0].split(":")[1];
             var endHour = str.split("-")[1].split(":")[0] - 1;
             var endMin = Number(str.split("-")[1].split(":")[1])+60;
-            obj.push({startHour, startMin, endHour, endMin})
             hours += (endHour - startHour + (endMin - startMin)/60)
         }
     }
 }
-console.log("仅计算工作日工时（已删除旷工）：" + hours/days);
+console.log("仅计算工作日工时（已删除旷工）：" +hours/days);
