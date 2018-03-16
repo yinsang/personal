835385d3634ad221b3512687fc2028a72a2ccac9
银 <love1175180338@gmail.com>
2018-03-16 19:32:17 +0800

    Create hours.js

---
+++ hours.js
@@ -0,0 +1,20 @@
+var doms = document.querySelectorAll(".detail-time")
+var obj = [];
+var hours = 0;
+var days = 0;
+console.error('计算');
+for (var i = 0; i < doms.length; i++) {
+    if (doms[i].parentElement.parentElement.parentElement.className !== "not-work-day") {
+        if (doms[i].innerHTML !== "-") {
+            days++;
+            var str = doms[i].innerHTML;
+            var startHour = str.split("-")[0].split(":")[0];
+            var startMin = str.split("-")[0].split(":")[1];
+            var endHour = str.split("-")[1].split(":")[0] - 1;
+            var endMin = Number(str.split("-")[1].split(":")[1])+60;
+            obj.push({startHour, startMin, endHour, endMin})
+            hours += (endHour - startHour + (endMin - startMin)/60)
+        }
+    }
+}
+console.log(hours/days);
